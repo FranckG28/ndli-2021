a9cca35f8421e329db9cb83b6bb8d091c3ae51c7
Eren Mustafa Ceylan <eren-mustafa.ceylan@etu.unistra.fr>
2021-12-03 01:40:20 +0100

    Added link in the nav bar

--- routes/web.php
+++ routes/web.php
@@ -19,52 +19,63 @@ Route::get('/', function () {
 
 Route::get('/home', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/boat', function () {
     return view('boat');
-});
+})->name('boat');
+
 
 Route::get('/boat/list', function () {
     return view('boatList');
-});
+})->name('boatList');
+
 
 Route::get('/resquer', function () {
     return view('resquer');
-});
+})->name('resquer');
+
 
 Route::get('/resquer/list', function () {
     return view('resquerList');
-});
+})->name('resquerList');
+
 
 Route::get('/saved', function () {
     return view('saved');
-});
+})->name('saved');
+
 
 Route::get('/saved/list', function () {
     return view('savedList');
-});
+})->name('savedList');
+
 
 Route::get('/trips', function () {
     return view('trips');
-});
+})->name('trips');
+
 
 Route::get('/trips/list', function () {
     return view('tripsList');
-});
+})->name('tripsList');
+
 
 Route::get('/community/boat', function () {
     return view('community.boat');
-});
+})->name('community.boat');
+
 
 Route::get('/community/resquer', function () {
     return view('community.resquer');
-});
+})->name('community.resquer');
+
 
 Route::get('/community/saved', function () {
     return view('community.saved');
-});
+})->name('community.saved');
+
 
 Route::get('/community/trips', function () {
     return view('community.trips');
-});
+})->name('community.trips');
